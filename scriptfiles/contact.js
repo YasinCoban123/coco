1cb405200351abcd154a3e2da1b9c6c51a44d8a2
YasinCoban123 <9018681@student.zadkine.nl>
2026-03-06 17:56:45 +0100

    form now works, contact fully finished

---
+++ scriptfiles/contact.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const form = document.getElementById("contact-form");
+  const modal = document.getElementById("success-modal");
+  const closeBtn = document.getElementById("close-modal");
+
+  if (!form) return;
+
+  form.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    try {
+      const response = await fetch("https://formspree.io/f/mreygklo", {
+        method: "POST",
+        body: data,
+        headers: {
+          "Accept": "application/json"
+        }
+      });
+
+      if (response.ok) {
+        modal.classList.remove("hidden");
+        modal.classList.add("flex");
+        form.reset();
+      } else {
+        alert("Er ging iets mis bij het versturen.");
+      }
+
+    } catch (error) {
+      alert("Netwerk fout. Probeer later opnieuw.");
+    }
+
+  });
+
+  closeBtn.addEventListener("click", function() {
+    modal.classList.add("hidden");
+  });
+
+});
